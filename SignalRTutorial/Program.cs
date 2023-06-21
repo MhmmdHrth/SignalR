51c4402cd0bdd778a57063e552985aea9fa5f42c
mhmmdhrth <harith781@gmail.com>
2023-06-21 11:20:26 +0800

    initial commit

---
+++ SignalRTutorial/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SignalRTutorial.Data;
+using SignalRTutorial.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+services.AddDatabaseDeveloperPageExceptionFilter();
+services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+services.AddControllersWithViews();
+services.AddSignalR();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+app.MapHub<UserHub>("/hubs/usesrCount");
+
+app.Run();
