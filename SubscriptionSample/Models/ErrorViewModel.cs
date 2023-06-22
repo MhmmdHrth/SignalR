c7ab036a1f08800156f2caf64a43989316591423
mhmmdhrth <harith781@gmail.com>
2023-06-22 10:33:04 +0800

    add subscription template

---
+++ SubscriptionSample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SubscriptionSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
