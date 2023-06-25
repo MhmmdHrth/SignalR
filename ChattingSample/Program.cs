4108e46d871f2976fd00dbe0cfd77dc172fa4e03
harith781 <harith781@gmail.com>
2023-06-25 17:32:59 +0800

    adding azure signalr

--- ChattingSample/Program.cs
+++ ChattingSample/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var azureSignalrConnectionString = "Endpoint=https://dotnetweb.service.signalr.net;AccessKey=6XMinM5SYH8UDdW8Bw8TLN9wpVxYo5i10X/IfM6Btt4=;Version=1.0;";
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -13,7 +14,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-builder.Services.AddSignalR();
+builder.Services.AddSignalR().AddAzureSignalR(azureSignalrConnectionString);
 
 var app = builder.Build();
 
